de7fa384388e5bbf5637f6c2d03baa341b42b63c
Travis Kent <travisokent@gmail.com>
2018-12-05 10:54:53 -0700

    Main menu
    added main menu and level resets

---
+++ Fox2/Assets/PlayerHealth.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+public int HP;
+public bool playerDead;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(HP <=0)
+		{
+			//signal player is dead and reload
+			playerDead = true;
+		}
+	}
+
+	public void TakeDamage()
+	{
+		HP = HP-1;
+	}
+	public void RecoverHealth()
+	{
+		HP = HP+1;
+	}
+}
