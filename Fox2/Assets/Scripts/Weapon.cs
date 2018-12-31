cfb5c2eeedb3241c1cb1ed5bd1d4504accc20b40
Travis Kent <travisokent@gmail.com>
2018-12-31 16:43:52 -0700

    asd

--- Fox2/Assets/Scripts/Weapon.cs
+++ Fox2/Assets/Scripts/Weapon.cs
@@ -11,7 +11,7 @@ public Slider engergySlider;
 	// Update is called once per frame
 	void Update () {
 
-		if(Input.GetKeyDown(KeyCode.Space))
+		if(Input.GetButtonDown("Fire1"))
 		{
 			Shoot();
 		}
